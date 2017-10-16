18d5aaafbd2f45c77c66f4fc6e89fddd6129d388
Anthony Mengel <anthonymengel1232@gmail.com>
2017-10-16 14:39:29 -0400

    added apiREPR to models js for restaurants

--- restaurants/models.js
+++ restaurants/models.js
@@ -3,32 +3,33 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const restaurantsSchema  = mongoose.Schema({
+const restaurantSchema  = mongoose.Schema({
     hours:{ type: String, required: true}, // ask if time is special input
     cuisine:{ type: String, required: true},
     price: {type: Number, required: true},
     address:{type: String, required: true},
     reservation: {type: Boolean, required: true},
-    title: {type: String, required: true},
-    Restaurants_URL:{type: String, required: true, unique: true},
-    Restaurants_Image_URL:{type: String, required: true, unique: true}
+    name: {type: String, required: true},
+    restaurant_URL:{type: String, required: true, unique: true},
+    restaurant_Image_URL:{type: String, required: true, unique: true}
 
   });
   
-  boardGameSchema.methods.apiRepr = function () {
+  restaurantSchema.methods.apiRepr = function () {
     return { 
-      avgRating:this.avgRating,
-      bgg_url:this.bgg_url,
-      imgUrl:this.imgUrl,
-      name: this.name,
-      minPlayers: this.minPlayers,
-      maxPlayers: this.maxPlayers,
-      avgTime: this.avgTime,
-      checked: this.checked
+        hours: this.hours,
+        cuisine: this.cuisine,
+        price: this.price,
+        address: this.address,
+        reservation: this.reservation,
+        name: this.name,
+        restaurant_URL: this.restaurant_URL,
+        restaurant_Image_URL: this.restaurant_Image_URL
+    
     };
   };
   
-  const BoardGame = mongoose.models.BoardGame || mongoose.model('BoardGame', boardGameSchema);
+  const Restaurant = mongoose.models.Restaurant || mongoose.model('Restaurant', restaurantSchema);
   
-  module.exports = { BoardGame };
+  module.exports = { Restaurant };
  
