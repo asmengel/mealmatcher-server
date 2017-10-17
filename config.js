4d1126acaec30e59f2aea8b75a93e6704bbfc1a0
Matt Lee <matthewlee07@gmail.com>
2017-10-17 00:17:34 -0400

    changed end point to meal-matcher

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/seed-rest',
+        process.env.DATABASE_URL || 'mongodb://localhost/meal-matcher',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/seed-rest-test'
+        'mongodb://localhost/meal-matcher-test'
 };
