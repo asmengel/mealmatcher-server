01f7fb373d28e5d96e39cf237fe820803576020e
Anthony Mengel <anthonymengel1232@gmail.com>
2017-10-18 16:36:12 -0400

    server related changed to reflect connection to front end

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const passport = require('passport');
 passport.use(jwtStrategy);
 const app = express();
 // endpoints
+app.use(function(req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Request-Headers");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api/auth/', authRouter);
 app.use('/api/restaurants/', restaurantsRouter);
 app.use('/api/users', usersRouter);
@@ -29,6 +36,9 @@ app.use(
         origin: CLIENT_ORIGIN
     })
 );
+
+
+
 
 function runServer(port = PORT) {
     const server = app
