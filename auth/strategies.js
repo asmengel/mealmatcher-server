a07c10bfb1b2decbe821b86d2f1bb352524c613f
Matt Lee <matthewlee07@gmail.com>
2017-10-18 15:01:30 -0400

    alex helping, sh frozen

--- auth/strategies.js
+++ auth/strategies.js
@@ -39,6 +39,7 @@ const jwtStrategy = new JwtStrategy(
   {
     secretOrKey: JWT_SECRET,
     jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+    algorithms: ['HS256']
   },
   (payload, done) => {
     done(null, payload.user);
