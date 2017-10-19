28b9646c65dd736600289144944ceb531afd7ff8
Matt Lee <matthewlee07@gmail.com>
2017-10-19 09:10:13 -0400

    changed yelp data to be compatible with postman

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const passport = require('passport');
 passport.use(jwtStrategy);
 const app = express();
 // endpoints
-app.use(function(req,res,next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Request-Headers");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -37,7 +37,102 @@ app.use(
     })
 );
 
-
+app.get('api/restaurantsdata', (req, res) => {
+    const restaurantArray = [
+        {
+            name: 'Burger & Lobster',
+            hours: '24 Hours',
+            cusine: 'Seafood, Burgers',
+            price: 20,
+            address: '39 W 19th St New York, NY 10011',
+            reservation: false,
+            restaurant_URL: 'https://www.yelp.com/biz/burger-and-lobster-new-york',
+            restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
+        }, {
+            name: 'Upstate',
+            hours: '24 Hours',
+            cusine: 'Seafood, Wine Bars, Beer Bar',
+            price: 20,
+            address: '95 1st Ave New York, NY 10003',
+            reservation: false,
+            restaurant_URL: 'https://www.yelp.com/biz/upstate-new-york-2',
+            restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
+        }, {
+            name: 'Sotto Le Stelle',
+            hours: '24 Hours',
+            cusine: 'Pizza',
+            price: 20,
+            address: '44-07 Queens Blvd Sunnyside, NY 11104',
+            reservation: false,
+            restaurant_URL: 'https://www.yelp.com/biz/sotto-le-stelle-sunnyside',
+            restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
+        }, {
+            name: 'Amélie',
+            hours: '24 Hours',
+            cusine: 'French, Wine Bars',
+            price: 20,
+            address: '22 W 8th St New York, NY 10011',
+            reservation: false,
+            restaurant_URL: 'https://www.yelp.com/biz/am%C3%A9lie-new-york',
+            restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
+        }, {
+            name: 'Joe’s Shanghai',
+            hours: '24 Hours',
+            cusine: 'Shanghainese, Desserts, Seafood',
+            price: 20,
+            address: '9 Pell St New York, NY 10013',
+            reservation: false,
+            restaurant_URL: 'https://www.yelp.com/biz/joes-shanghai-new-york-2',
+            restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
+        }, {
+            name: 'Cecconi’s Dumbo',
+            hours: '24 Hours',
+            cusine: 'Italian, Breakfast & Brunch, Bars',
+            price: 30,
+            address: '55 Water St Brooklyn, NY 11201',
+            reservation: false,
+            restaurant_URL: 'https://www.yelp.com/biz/cecconis-dumbo-brooklyn',
+            restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
+        }, {
+            name: 'Jacob’s Pickles',
+            hours: '24 Hours',
+            cusine: 'Comfort Food, Southern, American',
+            price: 20,
+            address: '509 Amsterdam Ave New York, NY 10024',
+            reservation: false,
+            restaurant_URL: 'https://www.yelp.com/biz/jacobs-pickles-new-york',
+            restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
+        }, {
+            name: 'Summerhill',
+            hours: '24 Hours',
+            cusine: 'Cocktail Bars, American (New), Tiki Bars',
+            price: 40,
+            address: '637 Nostrand Ave Brooklyn, NY 11216',
+            reservation: false,
+            restaurant_URL: 'https://www.yelp.com/biz/summerhill-brooklyn-4',
+            restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
+        }, {
+            name: 'Petra Bar & Restaurant',
+            hours: '24 Hours',
+            cusine: 'Bars, American',
+            price: 20,
+            address: '523 Evergreen Ave Brooklyn, NY 11221',
+            reservation: false,
+            restaurant_URL: 'https://www.yelp.com/biz/petra-bar-and-restaurant-brooklyn',
+            restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
+        }, {
+            name: 'Suffolk Arms',
+            hours: '24 Hours',
+            cusine: 'Cocktail Bars, Breakfast & Brunch, Gastropubs',
+            price: 20,
+            address: '269 Houston St, New York, NY 10002',
+            reservation: false,
+            restaurant_URL: 'http://www.suffolkarms.com/',
+            restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
+        },
+    ];
+    res.json(restaurantArray);
+})
 
 
 function runServer(port = PORT) {
--- yelp_data.js
+++ yelp_data.js
@@ -1,153 +1,110 @@
-'use strict';
-const express = require('express');
-
-const { PORT, DATABASE_URL } = require('./config');
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-const app = express();
-//const {Restaurant} = require('./restaurants);
-
-const restaurantArray =
-  [
-    {
-      name: 'Suffolk Arms',
-      hours: '5:00 pm - 2:00 am',
-      cusine: 'Cocktail Bars, Breakfast & Brunch, Gastropubs',
-      price: 20,
-      address: '269 Houston St, New York, NY 10002',
-      reservation: false,
-      restaurant_URL: 'http://www.suffolkarms.com/',
-      restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
-    },{
-      name: 'Upstate',
-      hours: '5:00 pm - 2:00 am',
-      cusine: 'Seafood, Wine Bars, Beer Bar',
-      price: 20,
-      address: '269 Houston St, New York, NY 10002',
-      reservation: false,
-      restaurant_URL: 'http://www.suffolkarms.com/',
-      restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
-    },{
-      name: 'Suffolk Arms',
-      hours: '5:00 pm - 2:00 am',
-      cusine: 'Cocktail Bars, Breakfast & Brunch, Gastropubs',
-      price: 20,
-      address: '269 Houston St, New York, NY 10002',
-      reservation: false,
-      restaurant_URL: 'http://www.suffolkarms.com/',
-      restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
-    },{
-      name: 'Best Breakfast',
-      hours: '5:00 pm - 2:00 am',
-      cusine: 'Cocktail Bars, Breakfast & Brunch, Gastropubs',
-      price: 20,
-      address: '269 Houston St, New York, NY 10002',
-      reservation: false,
-      restaurant_URL: 'http://www.suffolkarms.com/',
-      restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
-    },{
-      name: 'Later Lunch',
-      hours: '5:00 pm - 2:00 am',
-      cusine: 'Cocktail Bars, Breakfast & Brunch, Gastropubs',
-      price: 20,
-      address: '269 Houston St, New York, NY 10002',
-      reservation: false,
-      restaurant_URL: 'http://www.suffolkarms.com/',
-      restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
-    },{
-      name: 'Dope Dinner',
-      hours: '5:00 pm - 2:00 am',
-      cusine: 'Cocktail Bars, Breakfast & Brunch, Gastropubs',
-      price: 20,
-      address: '269 Houston St, New York, NY 10002',
-      reservation: false,
-      restaurant_URL: 'http://www.suffolkarms.com/',
-      restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
-    },{
-      name: 'Suffolk Arms',
-      hours: '5:00 pm - 2:00 am',
-      cusine: 'Cocktail Bars, Breakfast & Brunch, Gastropubs',
-      price: 20,
-      address: '269 Houston St, New York, NY 10002',
-      reservation: false,
-      restaurant_URL: 'http://www.suffolkarms.com/',
-      restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
-    },{
-      name: 'Yum House',
-      hours: '5:00 pm - 2:00 am',
-      cusine: 'Cocktail Bars, Breakfast & Brunch, Gastropubs',
-      price: 20,
-      address: '269 Houston St, New York, NY 10002',
-      reservation: false,
-      restaurant_URL: 'http://www.suffolkarms.com/',
-      restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
-    },{
-      name: 'Suffolk Arms2',
-      hours: '5:00 pm - 2:00 am',
-      cusine: 'Cocktail Bars, Breakfast & Brunch, Gastropubs',
-      price: 20,
-      address: '269 Houston St, New York, NY 10002',
-      reservation: false,
-      restaurant_URL: 'http://www.suffolkarms.com/',
-      restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
-    },{
-      name: 'Suffolk Arms3',
-      hours: '5:00 pm - 2:00 am',
-      cusine: 'Cocktail Bars, Breakfast & Brunch, Gastropubs',
-      price: 20,
-      address: '269 Houston St, New York, NY 10002',
-      reservation: false,
-      restaurant_URL: 'http://www.suffolkarms.com/',
-      restaurant_Image_URL: 'https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg'
-    },
-  ];
-
-
-function dbConnect(url = DATABASE_URL) {
-  return mongoose.connect(DATABASE_URL, { useMongoClient: true }).catch(err => {
-    console.error('Mongoose failed to connect');
-    console.error(err);
-  });
+{
+  "name": "Burger & Lobster",
+  "hours": "24 Hours",
+  "cuisine": "Seafood, Burgers",
+  "price": 20,
+  "address": "39 W 19th St New York, NY 10011",
+  "reservation": false,
+  "restaurant_URL": "https://www.yelp.com/biz/burger-and-lobster-new-york",
+  "restaurant_Image_URL": "https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg"
 }
 
-let server;
-
-function runServer(url = DATABASE_URL, port = PORT) {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(url, { useMongoClient: true }, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app
-        .listen(port, () => {
-          console.log(`Your app is listening on port ${port}`);
-          resolve();
-        })
-        .on('error', err => {
-          mongoose.disconnect();
-          reject(err);
-        });
-    });
-  });
+{
+  "name": "Upstate",
+  "hours": "24 Hours",
+  "cuisine": "Seafood, Wine Bars, Beer Bar",
+  "price": 20,
+  "address": "95 1st Ave New York, NY 10003",
+  "reservation": false,
+  "restaurant_URL": "https://www.yelp.com/biz/upstate-new-york-2",
+  "restaurant_Image_URL": "https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg"
 }
 
-return runServer()
-  .then(() => {
-    const arrayOfPromises = restaurantArray.map((restaurant, idx) => {
-      return Restaurant.create(restaurantArray[idx]);
-    });
-    return Promise.all(arrayOfPromises);
-  })
-  .then((restaurants) => {
-    // console.log(restaurants);
-    return restaurants.map(restaurant => restaurant._id);
-  })
-  .then(() => {
-    const arrayOfRestaurantPromises = restaurantArray.map((restaurant, idx) => {
-      return Restaurant.insertMany(restaurantArray[idx]);
-    });
-    return Promise.all(arrayOfQuestionPromises);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+{
+  "name": "Sotto Le Stelle",
+  "hours": "24 Hours",
+  "cuisine": "Pizza",
+  "price": 20,
+  "address": "44-07 Queens Blvd Sunnyside, NY 11104",
+  "reservation": false,
+  "restaurant_URL": "https://www.yelp.com/biz/sotto-le-stelle-sunnyside",
+  "restaurant_Image_URL": "https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg"
+}
+
+{
+  "name": "Amélie",
+  "hours": "24 Hours",
+  "cuisine": "French, Wine Bars",
+  "price": 20,
+  "address": "22 W 8th St New York, NY 10011",
+  "reservation": false,
+  "restaurant_URL": "https://www.yelp.com/biz/am%C3%A9lie-new-york",
+  "restaurant_Image_URL": "https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg"
+}
+
+{
+  "name": "Joe’s Shanghai",
+  "hours": "24 Hours",
+  "cuisine": "Shanghainese, Desserts, Seafood",
+  "price": 20,
+  "address": "9 Pell St New York, NY 10013",
+  "reservation": false,
+  "restaurant_URL": "https://www.yelp.com/biz/joes-shanghai-new-york-2",
+  "restaurant_Image_URL": "https://s3s-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg"
+}
+
+{
+  "name": "Cecconi’s Dumbo",
+  "hours": "24 Hours",
+  "cuisine": "Italian, Breakfast & Brunch, Bars",
+  "price": 30,
+  "address": "55 Water St Brooklyn, NY 11201",
+  "reservation": false,
+  "restaurant_URL": "https://www.yelp.com/biz/cecconis-dumbo-brooklyn",
+  "restaurant_Image_URL": "https://s3-smedia4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg"
+}
+
+{
+  "name": "Jacob’s Pickles",
+  "hours": "24 Hours",
+  "cuisine": "Comfort Food, Southern, American",
+  "price": 20,
+  "address": "509 Amsterdam Ave New York, NY 10024",
+  "reservation": false,
+  "restaurant_URL": "https://www.yelp.com/biz/jacobs-pickles-new-york",
+  "restaurant_Image_URL": "https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg"
+}
+
+{
+  "name": "Summerhill",
+  "hours": "24 Hours",
+  "cuisine": "Cocktail Bars, American (New), Tiki Bars",
+  "price": 40,
+  "address": "637 Nostrand Ave Brooklyn, NY 11216",
+  "reservation": false,
+  "restaurant_URL": "https://www.yelp.com/biz/summerhill-brooklyn-4",
+  "restaurant_Image_URL": "https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg"
+}
+
+{
+  "name": "Petra Bar & Restaurant",
+  "hours": "24 Hours",
+  "cuisine": "Bars, American",
+  "price": 20,
+  "address": "523 Evergreen Ave Brooklyn, NY 11221",
+  "reservation": false,
+  "restaurant_URL": "https://www.yelp.com/biz/petra-bar-and-restaurant-brooklyn",
+  "restaurant_Image_URL": "https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg"
+}
+
+
+{
+  "name": "Suffolk Arms",
+  "hours": "24 Hours",
+  "cuisine": "Cocktail Bars, Breakfast & Brunch, Gastropubs",
+  "price": 20,
+  "address": "269 Houston St, New York, NY 10002",
+  "reservation": false,
+  "restaurant_URL": "http://www.suffolkarms.com/",
+  "restaurant_Image_URL": "https://s3-media4.fl.yelpcdn.com/bphoto/fF1GcoJ6r9K3VckugcoZeg/180s.jpg"
+}
